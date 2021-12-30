7ff63fc27be924ff723ba6271e20d002465f9936
trevor <63176386+TfedUD@users.noreply.github.com>
2021-12-30 15:26:38 -0500

    feat: verified proper wall assignment

--- makedoe.py
+++ makedoe.py
@@ -7,11 +7,11 @@ from dragonfly.model import Model
 
 
 def main():
-    df_json = 'assets/reference_dfm/complex_model.dfjson'
+    df_json = 'assets/reference_dfm/complex_model_with_bcs.dfjson'
     out_inp = 'assets/test_output_inps'
-    out_file = pathlib.Path(out_inp, 'test-model.inp')
+    out_file = pathlib.Path(out_inp, 'test-model2.inp')
     df_model = Model.from_dfjson(dfjson_file=df_json)
-    a = model_to_inp(df_model, folder=out_inp, name='test-model.inp')
+    a = model_to_inp(df_model, folder=out_inp, name='test-model2.inp')
     a
 
 
